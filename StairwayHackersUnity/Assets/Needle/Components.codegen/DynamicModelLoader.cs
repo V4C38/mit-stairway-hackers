787193bf5fe055aff12abff9d8bf1205f73b72d1
Tobi <75667865+Alive-hub@users.noreply.github.com>
2025-01-25 16:40:26 -0500

    quick backup before I might break it all

---
+++ StairwayHackersUnity/Assets/Needle/Components.codegen/DynamicModelLoader.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class DynamicModelLoader : UnityEngine.MonoBehaviour
+	{
+		public void start(){}
+		public void loadModel(string @url){}
+		public void onDestroy(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
---
+++ StairwayHackersUnity/Assets/Needle/Components.codegen/UI_RecordStopButton.cs
@@ -0,0 +1,15 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class UI_RecordStopButton : UnityEngine.MonoBehaviour
+	{
+		public void startRecording(){}
+		public void stopRecording(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
